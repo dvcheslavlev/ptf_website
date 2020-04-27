65d9371976f6400c08b47f2e15978f92afe53005
noob-saibot <halo2olah90@gmail.com>
2020-04-27 19:56:36 +0300

    js part was added

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $(".btn-blue.like-btn").click(function(){
-      ga ('send', 'event', 'Like' , 'Click' , 'MainPageLike');
+    $(".feedback__submit").click(function(){
+      gtag('event', 'demo_like', {
+        'event_category': 'click',
+        'event_label': 'feedback'
     });
+      console.log('like');
+    });
+    $('.feedback-form__wrap').on('submit', function(e){
+                     // validation code here
+        e.preventDefault();
+        data = $(".feedback-form__wrap").serialize();
+        jQuery.post("script/info.php", data);
+        $(".feedback__submit").hide();
+        $(".feedback-form__wrap").append("<p class='thanks'>Thanks for leaving us your valuable feedback</p>");
+                   });
 });
